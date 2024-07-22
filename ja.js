d6c20d1d69c155966bfbc1dd0a6fcc3c13c3810e
pramoth-v-s <720821104081@hit.edu.in>
2024-07-22 16:38:26 +0530

    Add files via upload

---
+++ ja.js
@@ -0,0 +1,20 @@
+const a = document.getElementById("a")
+const b = document.getElementById("b")
+const c = document.getElementById("c")
+const d = document.getElementById("d")
+function D(){
+    d.style.backgroundColor = "rgb(30, 221, 30)"
+    d.style.color = "whitesmoke"
+}
+function A(){
+    a.style.backgroundColor = "red"
+    a.style.color = "whitesmoke"
+}
+function C(){
+    c.style.backgroundColor = "red"
+    c.style.color = "whitesmoke"
+}
+function B(){
+    b.style.backgroundColor = "red"
+    b.style.color = "whitesmoke"
+}
